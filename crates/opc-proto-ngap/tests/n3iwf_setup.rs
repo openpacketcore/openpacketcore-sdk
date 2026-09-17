4b4f6bc44fd8a4ab158253138e1f5ee951c8a84c
VerifiedOrganic <verifiedorganic@sent.com>
2026-09-16 23:05:52 -0600

    test(ngap): use MSRV-compatible array chunks in setup oracle
    Signed-off-by: VerifiedOrganic <verifiedorganic@sent.com>

--- crates/opc-proto-ngap/tests/n3iwf_setup.rs
+++ crates/opc-proto-ngap/tests/n3iwf_setup.rs
@@ -10,7 +10,9 @@ fn oracle() -> Value {
 fn octets(value: &str) -> Vec<u8> {
     value
         .as_bytes()
-        .chunks_exact(2)
+        .as_chunks::<2>()
+        .0
+        .iter()
         .map(|part| u8::from_str_radix(std::str::from_utf8(part).unwrap(), 16).unwrap())
         .collect()
 }
